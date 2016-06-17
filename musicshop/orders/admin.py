6c1141191292431c0b39fecbae856e52427ca74d
Pawel Kromplewski <pawel.kromplewski@pagepro.pl>
2016-06-17 13:48:16 +0200

    report

--- musicshop/orders/admin.py
+++ musicshop/orders/admin.py
@@ -1,6 +1,7 @@
+import csv
 from django.contrib import admin
 from musicshop.orders.models import Order, OrderProduct
-from django.db.models import Count
+from django.http import HttpResponse
 
 
 class OrderProductAdminInline(admin.TabularInline):
@@ -13,9 +14,28 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('client_name', 'address', 'status', 'get_total_price')
     fields = ('client_name', 'address', 'status', 'get_total_price')
     readonly_fields = ('get_total_price', )
+    actions = ('generate_report', )
 
     def get_total_price(self, obj):
-        return sum(map(lambda p: p.product.price * p.quantity, obj.order_products.all()))
+        return "{0:.2f}".format(sum(map(lambda p: p.product.price * p.quantity, obj.order_products.all())))
     get_total_price.short_description = 'total'
+
+    def generate_report(self, request, queryset):
+        response = HttpResponse(content_type="text/csv")
+        response['Content-Disposition'] = 'attachment; filename=report.csv'
+        writer = csv.writer(response)
+
+        writer.writerow(('ID zamówienia', 'Nazwa klienta', 'Adres', 'Status', 'Suma'))
+
+        for query in queryset:
+            record = []
+            for field in ('id', 'client_name', 'address', 'status', self.get_total_price):
+                if hasattr(field, '__call__'):
+                    item = field(query)
+                else:
+                    item = getattr(query, field, '')
+                record.append(item)
+            writer.writerow(record)
+        return response
 
 admin.site.register(Order, OrderAdmin)
